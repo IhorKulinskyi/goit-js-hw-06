699f42dca83ac37f77bd4f397192e1879cf53f72
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2023-03-21 23:36:52 +0100

    firsr commit

---
+++ js/task-02.js
@@ -0,0 +1,20 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const listEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map(ingredient => {
+  const liRef = document.createElement('li');
+  liRef.classList.add('item');
+  liRef.textContent = ingredient;
+  return liRef;
+} );
+
+listEl.append(...elements);
+
