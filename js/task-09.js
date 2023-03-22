396014453d06c98b3d3abc7614072595b7581331
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2023-03-22 17:16:55 +0100

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const bodyRef = document.querySelector("body");
+const currentColor = document.querySelector(".color");
+changeColorBtn.addEventListener("click", changeBodyColor);
+function changeBodyColor(event) {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  currentColor.textContent = getRandomHexColor();
+}
