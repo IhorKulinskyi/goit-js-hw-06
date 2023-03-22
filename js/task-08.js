51c7d212de86b4a9f449aedeb214a4b24a8f0a17
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2023-03-22 17:05:55 +0100

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const loginFormRef = document.querySelector(".login-form");
+
+loginFormRef.addEventListener("submit", onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  if (loginFormRef.elements.email.value === "" || loginFormRef.elements.password.value === "") {
+    alert("Заповніть усі рядки");
+    return;
+  }
+  const myFormData = new FormData(event.target);
+  const dataObj = Object.fromEntries(myFormData);
+  console.log(dataObj);
+  loginFormRef.reset();
+}
 
