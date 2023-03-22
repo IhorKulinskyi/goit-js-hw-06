68bdd618dba0a04001d638bf31ef2dce3cfb95ff
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2023-03-22 01:09:51 +0100

    task 4

---
+++ js/task-04.js
@@ -0,0 +1,25 @@
+let counterValue = 0;
+
+const decrementBtnEl = document.querySelector('button[data-action="decrement"]');
+// console.log(decrementBtnEl);
+
+const incrementBtnEl = document.querySelector('button[data-action="increment"]');
+// console.log(incrementBtnEl);
+
+const counterValueEl = document.querySelector('#value');
+// console.log(counterValueEl);
+
+decrementBtnEl.addEventListener('click',() => {
+    counterValue += -1;
+    counterValueEl.textContent=counterValue;
+});
+
+incrementBtnEl.addEventListener('click',() => {
+    counterValue += 1;
+    counterValueEl.textContent=counterValue;
+});
+
+
+
+
+
