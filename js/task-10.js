2a375bb254395691242ea289c1bb996e354e22a4
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2023-03-22 23:00:11 +0100

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBtnRef = document.querySelector("button[data-create]");
+const destroyBtnRef = document.querySelector("button[data-destroy]");
+const inputValue = document.querySelector("input");
+const boxesDiv = document.querySelector("#boxes");
+
+createBtnRef.addEventListener("click", createBoxes);
+destroyBtnRef.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  const elements = [];
+  let boxSide = 20;
+  amount = inputValue.value;
+  for (let i = 0; i < amount; i += 1) {
+    boxSide += 10;
+    const newElement = document.createElement("div");
+    newElement.style.backgroundColor = getRandomHexColor();
+    newElement.style.width = `${boxSide}px`;
+    newElement.style.height = `${boxSide}px`;
+    newElement.style.marginBottom = "10px";
+    elements.push(newElement);
+  }
+
+  boxesDiv.append(...elements);
+}
+
+function destroyBoxes() {
+  boxesDiv.replaceChildren();
+}
